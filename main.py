30c4f67b23b91e84bd2b96ea59de20534a54df37
Eren Mengeş <76516591+erenmenges@users.noreply.github.com>
2020-12-26 23:23:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+para = int(input("Faize koymak istediğiniz parayı girin:"))
+faizilk = int(input("Faiz oranını girin (nokta veya virgül koyma ve 4 basamaklı gir[örn:%11,25 --> 1125]):"))
+dolar_kuru = float(input("Dolar kurunu girin:"))
+paradolar = (para / dolar_kuru)
+places = 4
+faiz = faizilk * (10 ** (-1 * places))
+faizlipara = para + (para * faiz)
+faizliparadolar = faizlipara / dolar_kuru
+dolargelecek = dolar_kuru
+while (faizlipara/dolargelecek) >= (para/dolar_kuru):
+    dolargelecek += 0.01
+print("Eğer 1 sene sonra dolar kuru ", dolargelecek, " olursa zarar edersiniz.")
